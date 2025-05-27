9acecbbc54f7583d93b33bcdcbdde6ce1728b822
Jens von Bergmann <jens@mountainmath.ca>
2025-05-26 17:12:13 -0700

    include 2012, 2016, and 2019 SFS into simplified parsing.

--- R/sfs.R
+++ R/sfs.R
@@ -22,27 +22,44 @@ get_sfs_pumf <- function(pumf_version,pumf_cache_path) {
     pumf_base_path <- dir(pumf_base_path,full.names=TRUE)
   }
 
-  parse_pumf_metadata_spss(pumf_base_path,"bsweights")
-  parse_pumf_metadata_spss(pumf_base_path,"EFAM_PUMF_R")
-  parse_pumf_metadata_spss(pumf_base_path,"EFAM_PUMF_[^R]")
-
-
-  pumf_data <- read_pumf_data(pumf_base_path=pumf_base_path,layout_mask = "EFAM_PUMF_[^R]",
-                              file_mask = "EFAM_PUMF\\.txt") |>
-    mutate(across(c("PASRBUYG","PASRDWNG","PASRMPFG"),~str_pad(.x,width=2,side="left",pad="0")))
-
-  bsw_data <- read_pumf_data(pumf_base_path=pumf_base_path,layout_mask = "bsweights",
-                             file_mask = "SFS2023_BSWEIGHTS_PUMF\\.txt")
-  if ("PWEIGHT" %in% names(bsw_data)) {
-    bsw_data <- bsw_data |>
-      select(-"PWEIGHT")
+  weight_mask <- NULL
+  if (pumf_version=="2023") {
+    weight_mask <- "bsweights"
+    layout_mask <- "EFAM_PUMF_[^R]"
+    file_mask <- "EFAM_PUMF\\.txt"
+    parse_pumf_metadata_spss(pumf_base_path,"EFAM_PUMF_R")
+  } else if (pumf_version=="2019" || pumf_version=="2016") {
+    layout_mask <- "EFAM_PUMF"
+    file_mask <- layout_mask
+    weight_mask <- "bsweights"
+  } else if (pumf_version == "2012") {
+    layout_mask <- NULL
+    file_mask <- layout_mask
   }
-  pumf_data <- pumf_data |>
-    left_join(bsw_data,by="PEFAMID")
+  parse_pumf_metadata_spss(pumf_base_path,layout_mask)
 
+  pumf_data <- read_pumf_data(pumf_base_path=pumf_base_path,layout_mask = layout_mask,
+                              file_mask = file_mask)
+
+  if (pumf_version %in% c("2016","2019","2023")) { # fix issues with coding
+    pumf_data <- pumf_data |>
+      mutate(across(c("PASRBUYG","PASRDWNG","PASRMPFG"),~str_pad(.x,width=2,side="left",pad="0")))
+  }
+
+  if (!is.null(weight_mask)) {
+    parse_pumf_metadata_spss(pumf_base_path,weight_mask)
+    bsw_data <- read_pumf_data(pumf_base_path=pumf_base_path,layout_mask = weight_mask,
+                               file_mask = "BSWEIGHTS_PUMF\\.txt")
+    if ("PWEIGHT" %in% names(bsw_data)) {
+      bsw_data <- bsw_data |>
+        select(-"PWEIGHT")
+    }
+    pumf_data <- pumf_data |>
+      left_join(bsw_data,by="PEFAMID")
+  }
 
   attr(pumf_data,"pumf_base_path") <- pumf_base_path
-  attr(pumf_data,"layout_mask") <- "EFAM_PUMF_[^R]"
+  attr(pumf_data,"layout_mask") <- layout_mask
 
   pumf_data
 }
